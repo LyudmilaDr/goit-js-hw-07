2384d14de6d5fab281dddfa5be541a52854356e5
LyudmilaDr <110781702+LyudmilaDr@users.noreply.github.com>
2022-12-30 08:33:51 +0200

    готово

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryImages = document.querySelector(".gallery");
+const imagesMarkup = createPictureGallery(galleryItems);
+galleryImages.insertAdjacentHTML("beforeend", imagesMarkup);
+function createPictureGallery(galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__link" href = "${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`
+    })
+    .join("");
+}
+galleryImages.addEventListener("click",(event) => {
+event.preventDefault();
+if(event.target.nodeName !== "IMG"){
+    return;
+}
+const pictureBig = event.target.getAttribute("data-source");
+const instance = basicLightbox.create(`<img src="${pictureBig}"width="800" height="600">`);
+instance.show();
+});
+console.log(basicLightbox);
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryImages = document.querySelector(".gallery");
+const imagesMarkup = createPictureGallery(galleryItems);
+galleryImages.insertAdjacentHTML("beforeend", imagesMarkup);
+function createPictureGallery(galleryItems){
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}"
+         alt="${description}" />
+      </a>
+      </div>`
+    })
+    .join("");
+}
+let lightbox = new SimpleLightbox('.gallery a', {captionsData:'alt', captionPosition: 'bottom',captionDelay: 250, });
 
 console.log(galleryItems);
+console.log(SimpleLightbox);
